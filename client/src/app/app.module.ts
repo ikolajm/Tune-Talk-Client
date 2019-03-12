edd778083bbc1e67f218a8aefdfd94d148aa468a
Connor Bartlett <connorbartlett@Connors-MacBook-Pro.local>
2019-03-12 13:32:51 -0400

    user / router generated

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
