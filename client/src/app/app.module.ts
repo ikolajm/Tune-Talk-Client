1e111c22ac60599c24fa58cddbb97136f842f217
Matt Hawley <matthawley@Matts-MacBook-Pro.local>
2019-03-12 14:01:56 -0400

    push to connor

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/home/auth/login/login.component';
+import { SignupComponent } from './components/home/auth/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule
