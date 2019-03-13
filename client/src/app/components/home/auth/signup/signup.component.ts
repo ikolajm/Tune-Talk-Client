4b76a19416c0e217171e1ae46013ea1d4305841f
Matt Hawley <matthawley@Matts-MacBook-Pro.local>
2019-03-13 11:04:21 -0400

    signup comp

--- client/src/app/components/home/auth/signup/signup.component.ts
+++ client/src/app/components/home/auth/signup/signup.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
+// import { Router } from '@angular/router';
+// import { FormBuilder, FormGroup, } from '@angular/forms';
+// import { first } from 'rxjs/operators';
 
-@Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
-})
-export class SignupComponent implements OnInit {
+// // import { AlertService, UserService, AuthenticationService } from '../../../../_services/'
 
-  constructor() { }
+// @Component({
+//   selector: 'app-signup',
+//   templateUrl: './signup.component.html',
+//   styleUrls: ['./signup.component.css']
+// })
+// export class SignupComponent implements OnInit {
 
-  ngOnInit() {
-  }
+//   constructor(
+//     private router: Router,
+//     // private authenticationService: AuthenticationService,
+//     // private alertService: AlertService
+//   ) { 
+//     // if (this.authenticationService.currentUserValue) {
 
-}
+//     // }
+//   }
+
+//   ngOnInit() {
+//   }
+
+// }
