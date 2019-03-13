e625eacecb823422a6897b41cb9ba86ebc2ae9ad
Joe Crouse <yjcrouse@gmail.com>
2019-03-13 15:23:29 -0400

    Pulling from Jake

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/home/auth/login/login.component';
 import { SignupComponent } from './components/home/auth/signup/signup.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     HomeComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    
   ],
   imports: [
     BrowserModule,
