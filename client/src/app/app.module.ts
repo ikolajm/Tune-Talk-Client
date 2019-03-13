4543bad158a9462742ceba87caefdbd832670294
Joe Crouse <yjcrouse@gmail.com>
2019-03-13 16:44:54 -0400

    push updates

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
@@ -20,7 +21,8 @@ import { SignupComponent } from './components/home/auth/signup/signup.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
